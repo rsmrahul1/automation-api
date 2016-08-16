86ca84651a3149fc2366104ba07773c9d1dd860b
rsmrahul1 <rsmrahul@gmail.com>
2016-08-16 13:52:19 +0530

    Initial Draft structure

---
+++ automation-api/manage_app.py
@@ -0,0 +1,14 @@
+
+class ManageApp:
+    """ Implements the methods to manage apps """
+    def colse_app(app_id):
+        """ Close app by app ID in Android.
+
+        :Args:
+         - app_id - The unique app Id
+         
+        :Usage:
+            manageApp.colse_app('automation-test-app')
+        """
+        return True
+    
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+setup(
+    name = "automation_api",
+    version = "0.1",
+    packages = find_packages(),
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: test automation developers',
+        'Topic :: Test Automation :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+        ]
+    )
